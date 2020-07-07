46cc61f6bf16fe35c7e1fd6399582a7e63a657d3
ritu-21 <67868755+ritu-21@users.noreply.github.com>
2020-07-07 18:15:02 +0530

    Add files via upload

---
+++ MyCaptainProj1.2.cpp
@@ -0,0 +1,12 @@
+// simple C++ program to find the product of two numbers entered by the user.
+#include<iostream>
+using namespace std;
+int main()
+{
+	double i,j,s;
+	cout<<"Enter two numbers\n";  
+	cin >> i >>j;
+	s= i*j;
+	cout << "\n Product of the two numbers entered:"<< s;
+	return 0;
+}
